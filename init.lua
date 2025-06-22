94c278252de369091e6df9e23c7e1b0a17d4f2a6
chunyin7 <miles.c.y.ng@gmail.com>
2025-06-22 13:37:50 +1000

    ayu

--- init.lua
+++ init.lua
@@ -825,20 +825,17 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    'Shatur/neovim-ayu',
+    priority = 1000,
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
+      -- The `ayu` theme has multiple variants.
+      -- You can switch between them by passing `mirage = true` or `dark = true`.
+      -- If you don't pass anything, it defaults to the light theme.
+      require('ayu').setup {
+        mirage = false,
+        dark = false,
       }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'ayu'
     end,
   },
 
---
+++ lua/custom/plugins/ayu.lua
@@ -0,0 +1,3 @@
+return {
+    'shatur/neovim-ayu',
+}
